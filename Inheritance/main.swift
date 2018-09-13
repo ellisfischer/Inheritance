9fb8b4998a328a8f4392a553f8d661009ca69719
Fischer, Ellis Henry <efischer@hk223-27730.local>
2018-09-13 10:13:25 -0700

    Initial Commit

---
+++ Inheritance/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Inheritance
+//
+//  Created by Fischer, Ellis Henry on 9/13/18.
+//  Copyright © 2018 Fischer, Ellis Henry. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
